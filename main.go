82c2ffb77fd60fc8f3cfd5051f949cb5225dcf07
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-07-23 17:49:41 +0300

    Добавлены коментарий к методам и коду, переименована переменная urlHostVisitCount > hostVisitCount

--- main.go
+++ main.go
@@ -39,7 +39,8 @@ func main() {
 	}
 }
 
-func saveResponse(destinationPath string, url *url.URL, urlHostCounter map[string]int) error {
+// saveResponse - сохранение тела ответа от сервера по указанному URL
+func saveResponse(destinationPath string, url *url.URL, hostVisitCount map[string]int) error {
 	response, err := http.Get(url.String())
 	if err != nil {
 		return fmt.Errorf("сервер не отвечает URL: \"%s\" Ошибка: %s", url, err)
@@ -55,26 +56,30 @@ func saveResponse(destinationPath string, url *url.URL, urlHostCounter map[strin
 		return fmt.Errorf("ошибка чтения тела ответа URL:\"%s\" Ошибка: %s", url, err)
 	}
 
-	if err := saveResponseData(destinationPath, url, urlHostCounter, data); err != nil {
+	if err := saveResponseData(destinationPath, url, hostVisitCount, data); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func saveResponseData(destinationPath string, url *url.URL, urlHostCounter map[string]int, data []byte) error {
+// saveResponseData - сохранение данных тела ответа в файл
+func saveResponseData(destinationPath string, url *url.URL, hostVisitCount map[string]int, data []byte) error {
+	// Формирование имени файла с учетом текущего времени и количества запросов к хосту
 	now := time.Now().Format("2006-01-02_15-04-05")
-	fileName := fmt.Sprintf("%s_%d_%s", url.Host, urlHostCounter[url.Host]+1, now)
+	fileName := fmt.Sprintf("%s_%d_%s", url.Host, hostVisitCount[url.Host]+1, now)
 	filePath := filepath.Join(destinationPath, fileName)
 	err := os.WriteFile(filePath, data, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("ошибка записи данных тела ответа \"%s\" в файл \"%s\" Ошибка: %s", url, filePath, err)
 	}
 
-	urlHostCounter[url.Host]++
+	// Увеличение счетчика запросов к хосту
+	hostVisitCount[url.Host]++
 	return nil
 }
 
+// readUrlsFromFile - чтение слайса указателей на URL из файла
 func readUrlsFromFile(sourcePath string) ([]*url.URL, error) {
 	file, err := os.Open(sourcePath)
 	if err != nil {
@@ -85,6 +90,7 @@ func readUrlsFromFile(sourcePath string) ([]*url.URL, error) {
 	var urls []*url.URL
 	var urlCustom *url.URL
 	scanner := bufio.NewScanner(file)
+	// Чтение URL построчно
 	for scanner.Scan() {
 		urlCustom, err = url.Parse(scanner.Text())
 		if err != nil {
@@ -100,12 +106,15 @@ func readUrlsFromFile(sourcePath string) ([]*url.URL, error) {
 	return urls, nil
 }
 
+// createDirectory - создание директории для сохранения ответов
 func createDirectory(path string) error {
 	if err := os.MkdirAll(path, os.ModePerm); err != nil {
 		return fmt.Errorf("ошибка создания папки сохранения ответов по пути: \"%s\" Ошибка: \"%s\"", path, err)
 	}
 	return nil
 }
+
+// parseFlags - разбор флагов командной строки для получения путей к файлу с URL и папке для сохранения ответов
 
 func parseFlags() (string, string) {
 	var sourcePath string
